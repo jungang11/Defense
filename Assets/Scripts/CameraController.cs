8d237ddb39ab8edd3fbeaceb9084394aa11bac18
jungang11 <94428775+jungang11@users.noreply.github.com>
2023-06-07 16:08:46 +0900

    fix : CameraController 수정
    y 값의 조건문이 x 값으로 되어있어서 수정

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -43,16 +43,16 @@ public class CameraController : MonoBehaviour
     {
         Vector2 mousePos = value.Get<Vector2>();
 
-        if (mousePos.x <= padding)
+        if (mousePos.x <= 0 + padding)
             moveDir.x = -1;
         else if (mousePos.x >= Screen.width - padding)
             moveDir.x = 1;
         else
             moveDir.x = 0;
 
-        if (mousePos.y <= padding)
+        if (mousePos.y <= 0 + padding)
             moveDir.y = -1;
-        else if (mousePos.x >= Screen.height - padding)
+        else if (mousePos.y >= Screen.height - padding)
             moveDir.y = 1;
         else
             moveDir.y = 0;
